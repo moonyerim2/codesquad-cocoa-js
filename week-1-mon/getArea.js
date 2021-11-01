e1bdc42c02289aa3da6ffc3a6c48840362f19f88
moonyerim2 <moonyerim2@gmail.com>
2021-11-01 16:23:20 +0900

    add getRectArea, getTrapezoidArea, getArea function

--- week-1-mon/getArea.js
+++ week-1-mon/getArea.js
@@ -1,16 +1,42 @@
-function getCircleArea(r) {
+function getCircleArea(r, ...n) {
     const PI = 3.14;
 
     if (arguments[1] === undefined) {
         return r * r * PI;
     } else {
-        const n = arguments[1];
-        const start = arguments[0];
+        const startNum = r;
         let sum = 0;
 
-        for (let r = start; r <= n; r++) {
-            sum += r * r * PI;
+        for (let i = startNum; i <= n; i++) {
+            sum += i * i * PI;
         }
         return sum;
     }
 }
+
+function getRectArea(w, h) {
+    return w * h;
+}
+
+function getTrapezoidArea(upper, lower, h) {
+    return ((upper + lower) * h) / 2;
+}
+
+function getArea(figure) {
+    switch (figure) {
+        case "circle":
+            return getCircleArea(arguments[1], arguments[2]);
+        case "rect":
+            return getRectArea(arguments[1], arguments[2]);
+        case "trapezoid":
+            return getTrapezoidArea(arguments[1], arguments[2], arguments[3]);
+        default:
+            break;
+    }
+    console.log(arguments);
+}
+
+console.log(getArea("circle", 2, 3));
+console.log(getArea("circle", 10));
+console.log(getArea("rect", 10, 15));
+console.log(getArea("trapezoid", 10, 15, 12));
