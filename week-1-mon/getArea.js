d3aaafb2f0961c718ddec98d3520ad9e471e566c
moonyerim2 <moonyerim2@gmail.com>
2021-11-02 16:45:12 +0900

    add printExecutionSequence function

--- week-1-mon/getArea.js
+++ week-1-mon/getArea.js
@@ -1,36 +1,62 @@
-function getCircleArea(r, ...n) {
-    const PI = 3.14;
+const App = () => {
+    const figureLog = [];
+    const resultLog = [];
 
-    if (arguments[1] === undefined) {
-        return r * r * PI;
-    } else {
-        const startNum = r;
-        let sum = 0;
+    function getCircleArea(r, n = 0) {
+        const PI = 3.14;
 
-        for (let i = startNum; i <= n; i++) {
-            sum += i * i * PI;
+        if (n === 0) {
+            return r * r * PI;
+        } else {
+            const startNum = r;
+            let sum = 0;
+
+            for (let i = startNum; i <= n; i++) {
+                sum += i * i * PI;
+            }
+            return sum;
         }
-        return sum;
     }
-}
 
-function getRectArea(w, h) {
-    return w * h;
-}
-
-function getTrapezoidArea(upper, lower, h) {
-    return ((upper + lower) * h) / 2;
-}
-
-function getArea(figure) {
-    switch (figure) {
-        case "circle":
-            return getCircleArea(arguments[1], arguments[2]);
-        case "rect":
-            return getRectArea(arguments[1], arguments[2]);
-        case "trapezoid":
-            return getTrapezoidArea(arguments[1], arguments[2], arguments[3]);
-        default:
-            break;
+    function getRectArea(w, h) {
+        return w * h;
     }
-}
+
+    function getTrapezoidArea(upper, lower, h) {
+        return ((upper + lower) * h) / 2;
+    }
+
+    function printExecutionSequence() {
+        let result = "";
+
+        for (let i = 0; i < figureLog.length; i++) {
+            result += `${figureLog[i]} : ${resultLog[i]} \n`;
+        }
+
+        return result;
+    }
+
+    function getArea(figure) {
+        let result = 0;
+        figureLog.push(figure);
+
+        switch (figure) {
+            case "circle":
+                result = getCircleArea(arguments[1], arguments[2]);
+                resultLog.push(result);
+                break;
+            case "rect":
+                result = getRectArea(arguments[1], arguments[2]);
+                resultLog.push(result);
+                break;
+            case "trapezoid":
+                result = getTrapezoidArea(arguments[1], arguments[2], arguments[3]);
+                resultLog.push(result);
+                break;
+            default:
+                break;
+        }
+    }
+};
+
+App();
