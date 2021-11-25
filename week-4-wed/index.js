531e80492a939edac92d39087e16a35a6fc1d9b8
moonyerim2 <moonyerim2@gmail.com>
2021-11-25 18:04:21 +0900

    feat: problem2

--- week-4-wed/index.js
+++ week-4-wed/index.js
@@ -1,20 +1,82 @@
 const $menuContainer = document.querySelector("#menu-container");
+const $menuTitle = document.querySelector("#menu-title");
 const $menuList = document.querySelector("#menu-list");
-const classList = $menuList.classList
+const $mouseMoveOutput = document.querySelector("#mouse-move-output");
+const classList = $menuList.classList;
 
-let timer = null;
-
-const toggleMenuList = () => {
-    classList.toggle("hide-list");
+const removeHideListClass = () => {
+  classList.remove("hide-list");
 };
 
-$menuContainer.addEventListener("mouseover", () => {
-    timer = setTimeout(toggleMenuList, 1000);
-});
+const addHideListClass = () => {
+  classList.add("hide-list");
+};
 
-$menuContainer.addEventListener("mouseleave", () => {
+const getListItemTxt = (listItem) => {
+  if (listItem.classList.contains("menu-item")) {
+    const listItemTxt = listItem.innerText;
+    return listItemTxt;
+  }
+};
+
+const recordMouseMoveData = (object, key) => {
+  if (!object.hasOwnProperty(key)) {
+    object[key] = 1;
+  } else {
+    object[key] += 1;
+  }
+};
+
+const renderOutput = (dataObj) => {
+  let outputTemplate = "";
+
+  for (let key in dataObj) {
+    outputTemplate += `<p>${key}: ${dataObj[key]}<p>`;
+  }
+
+  $mouseMoveOutput.insertAdjacentHTML("beforeend", outputTemplate);
+};
+
+const eraseOutput = () => {
+  while ($mouseMoveOutput.hasChildNodes()) {
+    $mouseMoveOutput.removeChild($mouseMoveOutput.firstChild);
+  }
+};
+
+const App = () => {
+  let controllClassTimer = null;
+  let mouseMoveTimer = null;
+  let mouseMoveData = {};
+
+  $menuTitle.addEventListener("mouseover", () => {
+    controllClassTimer = setTimeout(removeHideListClass, 1000);
+  });
+
+  $menuList.addEventListener("mousemove", (e) => {
+    if (mouseMoveTimer) {
+      clearTimeout(mouseMoveTimer);
+    }
+
+    mouseMoveTimer = setTimeout(() => {
+      const listItem = e.target;
+      const key = getListItemTxt(listItem);
+
+      if (key !== undefined) {
+        eraseOutput();
+        recordMouseMoveData(mouseMoveData, key);
+      }
+      console.log(renderOutput(mouseMoveData));
+    }, 500);
+  });
+
+  $menuContainer.addEventListener("mouseleave", () => {
     if (classList.contains("hide-list")) {
-        clearTimeout(timer);
-    } else toggleMenuList();
-});
+      clearTimeout(controllClassTimer);
+    } else {
+      addHideListClass();
+      eraseOutput();
+    }
+  });
+};
 
+App();
