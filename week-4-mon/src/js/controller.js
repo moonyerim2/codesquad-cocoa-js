78456f5a512964933483cf792f502e1a525042ff
moonyerim2 <moonyerim2@gmail.com>
2021-11-25 14:33:25 +0900

    style: getNewTaskData() 반환 객체 포맷 수정

--- week-4-mon/src/js/controller.js
+++ week-4-mon/src/js/controller.js
@@ -1,9 +1,13 @@
 export class UserInputController {
-    constructor(userInput) {
-        this.userInput = userInput;
-    }
+  constructor(userInput) {
+    this.userInput = userInput;
+  }
 
-    getNewTaskData() {
-        return { taskID: Date.now(), task: this.userInput, checked: false };
-    }
+  getNewTaskData() {
+    return { 
+      taskID: Date.now(),
+      task: this.userInput,
+      checked: false 
+    };
+  }
 }
