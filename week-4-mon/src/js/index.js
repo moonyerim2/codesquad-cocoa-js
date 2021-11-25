5c8b847bea5601b4e2474e219cfa59bf6683830a
moonyerim2 <moonyerim2@gmail.com>
2021-11-25 12:18:40 +0900

    fix: keydown이벤트로 인한 사용자 입력이 한글일 경우 입력 값이 두 번 들어오는 버그를 keypress이벤트로 수정

--- week-4-mon/src/js/index.js
+++ week-4-mon/src/js/index.js
@@ -5,62 +5,62 @@ import { TaskManager } from "./model.js";
 const $ = (selector) => document.querySelector(selector);
 
 const preventFormDefault = (menuForm) => {
-    menuForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-    });
+  menuForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
 };
 
 const App = () => {
-    preventFormDefault($("#task-form"));
+  preventFormDefault($("#task-form"));
 
-    const $userInput = $("#input-new-task");
-    const $taskList = $("#task-list");
-    const $addBtn = $("#new-task-submit-button");
+  const $userInput = $("#input-new-task");
+  const $taskList = $("#task-list");
+  const $addBtn = $("#new-task-submit-button");
 
-    const taskManager = new TaskManager();
-    const viewManager = new ViewManager();
+  const taskManager = new TaskManager();
+  const viewManager = new ViewManager();
 
-    $userInput.addEventListener("keydown", (e) => {
-        if (e.key === "Enter") {
-            const userInputValue = new UserInputController($userInput.value);
-            const newTask = userInputValue.getNewTaskData();
-            taskManager.addTaskItem(newTask);
+  $userInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      const userInputValue = new UserInputController($userInput.value);
+      const newTask = userInputValue.getNewTaskData();
+      taskManager.addTaskItem(newTask);
 
-            viewManager.setDataList(taskManager.dataList);
-            viewManager.addListItem($taskList);
+      viewManager.setDataList(taskManager.dataList);
+      viewManager.addListItem($taskList);
 
-            $userInput.value = "";
-        }
-    });
+      $userInput.value = "";
+    }
+  });
 
-    $addBtn.addEventListener("click", (e) => {
-        const userInputValue = new UserInputController($userInput.value);
-        const newTask = userInputValue.getNewTaskData();
-        taskManager.addTaskItem(newTask);
+  $addBtn.addEventListener("click", (e) => {
+    const userInputValue = new UserInputController($userInput.value);
+    const newTask = userInputValue.getNewTaskData();
+    taskManager.addTaskItem(newTask);
 
-        viewManager.setDataList(taskManager.dataList);
-        viewManager.addListItem($taskList);
+    viewManager.setDataList(taskManager.dataList);
+    viewManager.addListItem($taskList);
 
-        $userInput.value = "";
-    });
+    $userInput.value = "";
+  });
 
-    $taskList.addEventListener("click", (e) => {
-        const targetNodeclassList = e.target.classList;
-        const taskItem = e.target.parentElement;
+  $taskList.addEventListener("click", (e) => {
+    const targetNodeclassList = e.target.classList;
+    const taskItem = e.target.parentElement;
 
-        if (targetNodeclassList.contains("delete-btn")) {
-            taskManager.deleteTaskItem(taskItem);
-            viewManager.eraseList($taskList);
-            viewManager.renderList($taskList);
-        } else if (targetNodeclassList.contains("edit-btn")) {
-            taskManager.editTaskItem(taskItem);
-            viewManager.eraseList($taskList);
-            viewManager.renderList($taskList);
-        } else if (targetNodeclassList.contains("done-task")) {
-            taskManager.checkDoneTask(taskItem);
-            viewManager.toggleDoneTaskClass(taskItem);
-        }
-    });
+    if (targetNodeclassList.contains("delete-btn")) {
+      taskManager.deleteTaskItem(taskItem);
+      viewManager.eraseList($taskList);
+      viewManager.renderList($taskList);
+    } else if (targetNodeclassList.contains("edit-btn")) {
+      taskManager.editTaskItem(taskItem);
+      viewManager.eraseList($taskList);
+      viewManager.renderList($taskList);
+    } else if (targetNodeclassList.contains("done-task")) {
+      taskManager.checkDoneTask(taskItem);
+      viewManager.toggleDoneTaskClass(taskItem);
+    }
+  });
 };
 
 App();
